7cf152a4d2feb296fca2aa9613d715beac0c5cc4
Matthias Metzger <noobymatze@yahoo.de>
2016-01-05 11:02:43 +0100

    Initial commit

---
+++ src/main/java/com/github/noobymatze/helloworld/HelloWorldApplication.java
@@ -0,0 +1,8 @@
+package com.github.noobymatze.helloworld;
+
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
